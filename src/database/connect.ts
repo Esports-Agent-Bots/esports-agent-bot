4ca645a37a7fc5ee2a37d655dacc79dd523a434b
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-19 20:12:05 -0700

    feat: database connection
    Add handler to connect to database.

---
+++ src/database/connect.ts
@@ -0,0 +1,23 @@
+import { connect } from "mongodb";
+import { errorHandler } from "../helpers/errorHandler";
+import { logHandler } from "../helpers/logHandler";
+
+export const connectDatabase = async (): Promise<void> => {
+  try {
+    const dbUri = process.env.MONGO_URI;
+    if (!dbUri) {
+      logHandler.log("error", "missing DB connection string.");
+      return;
+    }
+    await connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    logHandler.log("debug", "database connected!");
+
+    return;
+  } catch (error) {
+    errorHandler("database connection", error);
+  }
+};
--- src/events/onReady.ts
+++ src/events/onReady.ts
@@ -1,7 +1,9 @@
+import { connectDatabase } from "../database/connect";
 import { logHandler } from "../helpers/logHandler";
 import { startServer } from "../helpers/server";
 
 export const onReady = async (): Promise<void> => {
   await startServer();
+  await connectDatabase();
   logHandler.log("debug", "Bot is online!");
 };
