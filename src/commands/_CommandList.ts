8b86335e16be02dbf92f5b55d14e5bcc175c8cc1
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-27 16:35:31 -0700

    feat: help command
    A brief help command to generate a list of available features.

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,6 +1,7 @@
 import { CommandInt } from "../interfaces/CommandInt";
 import { about } from "./about";
+import { help } from "./help";
 import { notification } from "./notification";
 import { ping } from "./ping";
 
-export const CommandList: CommandInt[] = [about, notification, ping];
+export const CommandList: CommandInt[] = [about, help, notification, ping];
