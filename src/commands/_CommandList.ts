58012166dbaceaf468da469a741c4d14c5a4e1ff
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-04-27 13:17:53 -0700

    feat: notification command
    Set up the initial infrastructure for the notification command.

--- src/commands/_CommandList.ts
+++ src/commands/_CommandList.ts
@@ -1,4 +1,5 @@
 import { CommandInt } from "../interfaces/CommandInt";
+import { notification } from "./notification";
 import { ping } from "./ping";
 
-export const CommandList: CommandInt[] = [ping];
+export const CommandList: CommandInt[] = [notification, ping];
---
+++ src/commands/notification.ts
@@ -0,0 +1,30 @@
+import { CommandInt } from "../interfaces/CommandInt";
+
+export const notification: CommandInt = {
+  name: "notification",
+  description: "Create, update, view, or delete a notification.",
+  run: async (message) => {
+    const { content } = message;
+
+    const [, , action] = content.split(" ");
+
+    switch (action) {
+      case "create":
+        // run create handler
+        break;
+      case "update":
+        // run update handler
+        break;
+      case "view":
+        // run view handler
+        break;
+      case "delete":
+        // run delete handler
+        break;
+      default:
+        await message.reply(
+          `Sorry, but ${action} is not a valid method for this command.`
+        );
+    }
+  },
+};
